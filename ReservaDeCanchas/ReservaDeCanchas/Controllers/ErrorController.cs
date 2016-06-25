516b93487004ff63e9b5dc6bedd7a8fb1b06126c
rickyrish <the.ricky.rish@gmail.com>
2016-06-25 12:17:54 -0500

    paginas de error

---
+++ ReservaDeCanchas/ReservaDeCanchas/Controllers/ErrorController.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace ReservaDeCanchas.Controllers
+{
+    public class ErrorController : Controller
+    {
+        // GET: Error
+        public ActionResult Index404()
+        {
+            return View();
+        }
+
+        public ActionResult Index500()
+        {
+            return View();
+        }
+
+        public ActionResult Index401()
+        {
+            return View();
+        }
+    }
+}
